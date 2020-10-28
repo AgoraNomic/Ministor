0fb30e6ebdc60a50f76c73a1135a5f1c86a66687
nixnull <nixnull@protonmail.com>
2020-10-27 22:17:36 -0500

    added pot total

--- generate.py
+++ generate.py
@@ -8,14 +8,15 @@ legacy_players.remove(winner)
 runner_ups = ", ".join(legacy_players)
 
 # Calculate Econ stuff
-boatloads = 1.1
-econ_pot = 50 * boatloads
+boatloads = 2.38
+econ_pot = 51 * boatloads
 econ_players = 5
 econ_split = int(ceil(econ_pot / econ_players))
 
 # The map
 mapping = {'winner': winner, 'runner_ups': runner_ups, 
-           'econ_players': econ_players, 'econ_split': econ_split}
+           'econ_players': econ_players, 'econ_split': econ_split,
+           'econ_pot': round(econ_pot,2)}
 
 # Apply the map we built above to the template.
 with open('template.txt', 'r') as infile:
